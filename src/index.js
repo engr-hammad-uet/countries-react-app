fa90dd0e49ebe012fccc3c7342d7bb5cd04dbd48
Hammad Ul Hassan <64351269+engr-hammad-uet@users.noreply.github.com>
2021-09-13 04:29:17 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstName: 'John',
+      data: [],
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  componentDidMount() {
+    const API_URL = 'https://restcountries.eu/rest/v2/all'
+    fetch(API_URL)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          data,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  renderCountries = () => {
+    return this.state.data.map((country) => {
+      return (
+        <div className="inner-div">
+          <div>
+            {' '}
+            <img src={country.flag} alt={country.name} />{' '}
+          </div>
+          <div className="outer-flex">
+            <div className="flex"><p>Name: </p><p>{country.name}</p></div>
+            <div className="flex"><p>Population: </p><p>{country.population}</p></div>
+            <div className="flex"><p>Capital: </p><p>{country.capital}</p></div>
+
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+
+      <div className="outer-container">
+        <div className='text-center'>
+          <h1>Find Your Country</h1>
+          <p>There are {this.state.data.length} countries in the world</p>
+          <p>Which one is yours?</p>
+        </div>
+        <div className='App'>
+          <div className='countries-wrapper-inner'>{this.renderCountries()}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
+
 serviceWorker.unregister();
